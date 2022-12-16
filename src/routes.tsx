3b59017da233a3fd2b5c6f06c086d65b8da6401e
maaza4u <98598449+maaza4u@users.noreply.github.com>
2022-12-16 18:36:49 +0530

    Update routes.tsx
    for responsive view

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,9 +2,17 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import AddUser from './pages/AddUser';
 import EditUser from './pages/EditUser';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+
+  
+
 
 const Routes = () => {
+  const matches = useMediaQuery('(min-width:600px)');
   return (
+    <span>
+      {`(min-width:600px) matches: ${matches}`}
     <div>
     <BrowserRouter>
       <Switch>
@@ -14,7 +22,9 @@ const Routes = () => {
       </Switch>
     </BrowserRouter>
     </div>
+    </span>
   );
 };
 
 export default Routes;
+
