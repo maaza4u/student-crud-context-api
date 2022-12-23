c4860b9d6a12d84d3c0658197968cfd3c3087c0d
maaza4u <98598449+maaza4u@users.noreply.github.com>
2022-12-23 23:36:50 +0530

    Update EditUser.tsx

--- src/pages/EditUser.tsx
+++ src/pages/EditUser.tsx
@@ -4,8 +4,17 @@ import { Link, useHistory, useParams } from 'react-router-dom';
 import { useGlobalContext } from '../contexts/GlobalState';
 import { IUser, IParams } from '../types';
 
-const EditUser: React.FC = () => {
+type Props = { 
+  data: IUser;
+  onBackBtnClickHnd: () => void;
+  users : IUser [];
+  setUsers: (data : IUser []) => void;
+  onUpdateClickHnd: (data: IUser) => void;
+}
+
+const EditUser = (props:Props) => {
   const history = useHistory();
+  const {onBackBtnClickHnd,onUpdateClickHnd} = props
 
   const { currentState, editUser } = useGlobalContext();
   const users = currentState.users;
@@ -32,9 +41,10 @@ const EditUser: React.FC = () => {
     event.preventDefault();
 
     editUser(selectedUser);
-
+    onBackBtnClickHnd();
     console.log('new user edited:', selectedUser);
     history.push('/');
+    onUpdateClickHnd(selectedUser);
   }
 
   const onChangeEditName = (
@@ -45,6 +55,82 @@ const EditUser: React.FC = () => {
       [e.target.name]: e.target.value,
     });
   };
+
+  const onChangeEditEmail = (
+    e:any
+  ) => {
+    setSelectedUser({
+      ...selectedUser,
+      [e.target.email]: e.target.value,
+    });
+  };
+
+  const onChangeEditPhone = (
+    e:any
+  ) => {
+    setSelectedUser({
+      ...selectedUser,
+      [e.target.phone]: e.target.value,
+    });
+  };
+
+  return (
+    <>
+      <Grid>
+        <Container>
+      <h3>Edit Student Info</h3>
+
+      <form onSubmit={onSubmit}>
+        <FormControl className="mb-2">
+          <div>
+          <FormLabel>Name :</FormLabel>
+          <Input
+            type="text"
+            name="name"
+            onChange={onChangeEditName}
+            placeholder="Enter new name"
+            required
+          />
+          </div>
+          <div>
+          <FormLabel>Email :</FormLabel>
+          <Input
+            type="text"
+            name="email"
+            onChange={onChangeEditEmail}
+            placeholder="Enter New Email"
+            required
+          />
+          </div>
+          <div>
+          <FormLabel>Email :</FormLabel>
+          <Input
+            type="text"
+            name="email"
+            onChange={onChangeEditPhone}
+            placeholder="Enter New Phone Number"
+            required
+          />
+          </div>
+        </FormControl>
+        <div>
+        <Button type="submit" variant='contained' color='primary' style={{margin:'5px'}}>
+          Submit
+        </Button>
+        <Button variant='contained' color='secondary'>
+        <Link to="/">
+          Cancel
+        </Link>
+        </Button>
+        </div>
+      </form>
+      </Container>
+      </Grid>
+    </>
+  );
+};
+
+export default EditUser;
 
   const onChangeEditEmail = (
     e:any
