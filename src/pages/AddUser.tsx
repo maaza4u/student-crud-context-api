5105cdebd0303ed85790c4d581a139d17d1ad123
maaza4u <98598449+maaza4u@users.noreply.github.com>
2022-12-27 10:34:00 +0530

    Update AddUser.tsx
    validation done , but some update issue is there

--- src/pages/AddUser.tsx
+++ src/pages/AddUser.tsx
@@ -5,7 +5,8 @@ import { useGlobalContext } from '../contexts/GlobalState';
 import { IUser } from '../types';
 import { FormControl,FormLabel,Input,Button, Grid, Container} from '@material-ui/core';
 import * as Yup from 'yup';
-import { Formik, Form, Field } from 'formik';
+import { Form, Field,useFormik } from 'formik';
+import { schemaValidation } from "../validationschema";
 
 type Props = {
 
@@ -15,17 +16,12 @@ type Props = {
   handleSubmit:any
 }
 
-const SignupSchema = Yup.object().shape({
-  name: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
- email: Yup.string().email('Invalid email').required('Required'),
- phone: Yup.string()
- .min(2, 'Too Short!')
- .max(50, 'Too Long!')
- .required('Required'),
-});
+const initialValues : IUser = {
+  id: uuid(),
+  name:'',
+  email:'',
+  phone:''
+}
 
 
 
@@ -39,26 +35,26 @@ const AddUser = (props: Props) => {
   const { addUser } = useGlobalContext();
   const{users,setUsers,onBackBtnClickHnd} = props
 
-  const onSubmit =(values:any) => {
-    const newUser: IUser = {
-      id: uuid(),
-      name,
-      email,
-      phone
-    };
+  // const onSubmit =(values:any) => {
+  //   const newUser: IUser = {
+  //     id: uuid(),
+  //     name,
+  //     email,
+  //     phone
+  //   };
 
-    addUser(newUser);
+  //   addUser(newUser);
 
-    // console.log('new user added:', addUser);
-    history.push('/');
-    values.id = users.length + 1;
+  //   // console.log('new user added:', addUser);
+  //   history.push('/');
+  //   values.id = users.length + 1;
       
-    setUsers([...users,values])
-    onBackBtnClickHnd();
+  //   setUsers([...users,values])
+  //   onBackBtnClickHnd();
        
-    // same shape as initial values
-    console.log(values);
-  }
+  //   // same shape as initial values
+  //   console.log(values);
+  // }
 
   // function onSubmitt() {
   //   const newUser: IUser = {
@@ -91,7 +87,27 @@ const AddUser = (props: Props) => {
     setPhone(e.target.value);
   }
 
+  const { values, errors, touched, handleBlur, handleChange, handleSubmit } = useFormik({
+    initialValues,
+    // validateOnMount:true,
+    validationSchema: schemaValidation,
+    // enableReinitialize:true,
+    onSubmit: (values, action) => {
   
+      values.id = users.length + 1;
+    
+      setUsers([...users,values])
+      // onSubmitClickHnd(values);
+      console.log(
+        "valueone",
+        users
+      );
+      action.resetForm();
+      addUser(initialValues);
+     
+
+    },
+  });
 
 
 
@@ -101,30 +117,13 @@ const AddUser = (props: Props) => {
         <Container>
       <h3 style={{color:'black'}}>ADD STUDENT INFO</h3>
 
-      <Formik
-       initialValues={{
-         name: '',
-         email: '',
-         phone: '',
-       }}
-       validationSchema={SignupSchema}
-       onSubmit={onSubmit}
-       onBackBtnClickHnd={onBackBtnClickHnd}
-
-       
-      //  onSubmit={values => {
-  
-      //    // same shape as initial values
-      //    console.log(values);
-      //  }}
-     >
-       {({ errors, touched,values,handleBlur,handleChange,handleSubmit }) => (
+      
       <form onSubmit={handleSubmit}>
         <FormControl className="mb-2">
           <div>
           <FormLabel>Name : </FormLabel>
            
-          <Field
+          <Input
             id='name'
             name='name'
             type="name"
@@ -132,11 +131,12 @@ const AddUser = (props: Props) => {
             value={values.name}
             onBlur={handleBlur}
             placeholder="Enter name"
+            autoComplete='of' 
             required
           />
-           {errors.name && touched.name ? (
-             <div style={{color:'red',fontSize:'15px',width:'15%',paddingLeft:'120px',paddingTop:'10px'}} >{errors.name}</div>
-           ) : null}
+            {touched.name && errors.name ? (
+            <div style={{color:'red',fontSize:'15px',width:'100%',paddingLeft:'50px',paddingTop:'10px'}} >{errors.name}</div>
+          ) : null} 
     
           </div>
           <div>
@@ -164,7 +164,7 @@ const AddUser = (props: Props) => {
         </FormControl>
         <div>
         
-        <Button type="submit" variant="contained" color="primary" style={{margin:'5px'}}>Submit</Button>
+        <Button type='submit' variant="contained" color="primary" style={{margin:'5px'}}>Submit</Button>
         
     
         <Button type='button' variant="contained" color='secondary'>
@@ -175,8 +175,7 @@ const AddUser = (props: Props) => {
         </div>
 
       </form>
-        )}
-      </Formik>
+
       
       </Container>
       </Grid>
@@ -187,9 +186,5 @@ const AddUser = (props: Props) => {
 
 
 export default AddUser;
-
-
-
-
 
 
